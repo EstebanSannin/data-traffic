0569833801201c09a3e1c759b713716b441a842d
Stefano Viola <stefanoviola@sannioglug.org>
2010-05-20 10:01:54 +0200

    added the License

--- implements.c
+++ implements.c
@@ -1,10 +1,14 @@
 /*
  *
  * AUTHOR:   Stefano Viola (aka) Esteban Sannin
- * CONTACT:  stefanoviola@sannioglug.org
- * HOME:     http://esteban.homelinux.org
- * LICENSE:  GPLv3
+ * CONTACT:  stefanoviola[at]sannioglug[dot]org
+ * HOME:     http://esteban.homelinux.org   
  *
+ *      Licenze GLPv3
+ *      This program is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU General Public License
+ *      as published by the Free Software Foundation; either version
+ *      3 of the License, or (at your option) any later version.
  *
  */
 
